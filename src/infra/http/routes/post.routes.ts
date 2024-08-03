3625c500d4157958785954cccc88ea1fb9ceba82
BrunoRMello <mello.bruno@live.com>
2024-08-03 15:17:36 -0300

    feat: alterado rota de criar posts com usuario logado

--- src/infra/http/routes/post.routes.ts
+++ src/infra/http/routes/post.routes.ts
@@ -1,17 +1,29 @@
 import { celebrate, Joi, Segments } from 'celebrate';
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
+
+import { authenticationHub } from '@/shared/infra/http/middlewares/authenticationHub';
+import { ensureAuthenticated } from '@/shared/infra/http/middlewares/ensureAuthenticated';
 
 import { PostController } from '../controller/PostController';
 
 const postRouter = Router();
 const postController = new PostController();
 
+const middlewareWrapper = (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) =>
+  authenticationHub(request, response, next, {
+    authMiddlewares: [ensureAuthenticated],
+  });
+
 postRouter.post(
   '/',
+  middlewareWrapper,
   celebrate({
     [Segments.BODY]: {
       name: Joi.string().required(),
-      userId: Joi.string().required(),
       description: Joi.string(),
       image: Joi.string().required(),
     },
